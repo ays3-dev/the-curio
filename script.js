24a276e842adbc014638568aa01a3a4045cad385
ays3-dev <yaraysenaz@gmail.com>
2026-03-03 18:19:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -677,23 +677,3 @@ cleanHistoryBtn.addEventListener('click', async () => {
     }
 });
 
-const darkToggle = document.getElementById('dark-toggle');
-
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-mode');
-    darkToggle.innerText = 'Light Mode';
-}
-
-darkToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    
-    if (document.body.classList.contains('dark-mode')) {
-        darkToggle.innerText = 'Light Mode';
-        localStorage.setItem('theme', 'dark'); 
-    } else {
-        darkToggle.innerText = 'Dark Mode';
-        localStorage.setItem('theme', 'light'); 
-    }
-
-});
-
